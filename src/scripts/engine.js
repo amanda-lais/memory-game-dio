4f12ab7a22d495b69dbd87132569eeac73cb70cb
Amanda Laís <amandalais037@gmail.com>
2024-01-21 01:01:00 -0300

    criar cards dinamicamente

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -22,5 +22,7 @@ let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
 
 for(let i=0; i < emojis.length; i++){
     let box = document.createElement("div");
-
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    document.querySelector(".game").appendChild(box);
 }
