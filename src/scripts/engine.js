fb5a7c705dc199512cdb7fdce5398c32eb3758dd
Amanda Laís <amandalais037@gmail.com>
2024-01-21 00:57:47 -0300

    início js

---
+++ src/scripts/engine.js
@@ -0,0 +1,26 @@
+const emojis = [
+    "🦊",
+    "🦊",
+    "🕷️",
+    "🕷️",
+    "💵",
+    "💵",
+    "🦝",
+    "🦝",
+    "😺",
+    "😺",
+    "🐶",
+    "🐶",
+    "🐺",
+    "🐺",
+    "🐯",
+    "🐯",
+];
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+for(let i=0; i < emojis.length; i++){
+    let box = document.createElement("div");
+
+}
